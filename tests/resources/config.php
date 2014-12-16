b84e7cc76b7e5db87ea8aabd77417f72162f47ab
Tom Kaczocha <tom@rawphp.org>
2014-12-16 20:42:54 +1100

    Remove PHP 5.3 compat.

--- tests/resources/config.php
+++ tests/resources/config.php
@@ -1,17 +1,15 @@
 <?php
 
-return
-    [
-        'from_email' => 'no-reply@rawphp.org',
-        'from_name'  => 'RawPHP',
-        'smtp'       => array(
+return [
+    'from_email' => 'no-reply@rawphp.org',
+    'from_name'  => 'RawPHP',
+    'smtp'       => [
 
-            'auth'     => TRUE,
-            'host'     => 'smtp.gmail.com',
-            'username' => 'username',
-            'password' => 'password',
-            'security' => 'ssl',
-            'port'     => 465,
-        )
-
-    ];
+        'auth'     => TRUE,
+        'host'     => 'smtp.gmail.com',
+        'username' => 'username',
+        'password' => 'password',
+        'security' => 'ssl',
+        'port'     => 465,
+    ]
+];
